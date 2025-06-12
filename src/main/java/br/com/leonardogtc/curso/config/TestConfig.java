17b3f4a4090eeaa1eb88ffdfa3b4d3ec02e3d56e
Leonardo Gonçalves Tavares da Conceição <leonardogtc@gmail.com>
2025-06-12 07:55:13 -0300

    OrderStatus enum

--- src/main/java/br/com/leonardogtc/curso/config/TestConfig.java
+++ src/main/java/br/com/leonardogtc/curso/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import br.com.leonardogtc.curso.entities.Order;
 import br.com.leonardogtc.curso.entities.User;
+import br.com.leonardogtc.curso.entities.enums.OrderStatus;
 import br.com.leonardogtc.curso.repositories.OrderRepository;
 import br.com.leonardogtc.curso.repositories.UserRepository;
 
@@ -19,22 +20,22 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		User u1 = new User(null, "Oliver", "oliverptc@gmail.com", "11999999999", "123456");
 		User u2 = new User(null, "Lucia", "luciaptc@gmail.com", "11999999998", "123456");
-		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
-		
+
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		
+
 	}
 }
